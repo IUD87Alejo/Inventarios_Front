392e7b0b34dba7750a3532b2fa2a1ce92c4cc550
David Ruiz <davidcoin87@gmail.com>
2022-06-16 18:36:10 -0500

    Cambios en el front
    Cambios en el front

--- front/src/index.js
+++ front/src/index.js
@@ -5,9 +5,3 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-
-let port = process.env.PORT;
-if(port === null || port === "") {
-    port = 3000;
-}
