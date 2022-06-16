accdad6d992ffbf8228e7a716e7105547f802c06
David Ruiz <davidcoin87@gmail.com>
2022-06-15 21:23:43 -0500

    Update axios-config.js
    Salida a Heroku

--- front/src/helpers/axios-config.js
+++ front/src/helpers/axios-config.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL:'http://localhost:9000/api/'
+    // baseURL:'http://localhost:9000/api/'
+    baseURL:'https://inventariobackend.herokuapp.com/api/'
+    
 });
 
 export {
